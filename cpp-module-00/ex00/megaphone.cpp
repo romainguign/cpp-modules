869a688989a5b7c68e8142dcd5fdd4bca846e23f
romainguign <guignard.romain@protonmail.com>
2024-06-11 17:17:59 +0200

    ex00 OK

--- cpp-module-00/ex00/megaphone.cpp
+++ cpp-module-00/ex00/megaphone.cpp
@@ -6,16 +6,28 @@
 /*   By: roguigna <roguigna@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/11 16:12:01 by roguigna          #+#    #+#             */
-/*   Updated: 2024/06/11 16:23:44 by roguigna         ###   ########.fr       */
+/*   Updated: 2024/06/11 17:17:24 by roguigna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+#include <string>
+
 int main (int argc, char **argv)
 {
-	if (argc != 2)
+	if (argc == 1)
 	{
-		
-		return (1);
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return (0);
 	}
-		
+	argv++;
+	for ( ; *argv; argv++)
+	{
+		std::string str = *argv;
+		for (int i = 0; str[i] ; i++)
+			str[i] = std::toupper(str[i]);
+		std::cout << str;
+	}
+	std::cout << std::endl;
+	return (0);
 }
