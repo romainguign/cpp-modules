93ec0b4173033873fe4e57846f3d34b3a9ee1c71
romainguign <guignard.romain@protonmail.com>
2024-09-24 15:05:39 +0200

    finishe

--- cpp-module-01/ex03/HumanB.cpp
+++ cpp-module-01/ex03/HumanB.cpp
@@ -6,16 +6,11 @@
 /*   By: roguigna <roguigna@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/18 16:48:16 by roguigna          #+#    #+#             */
-/*   Updated: 2024/07/24 12:26:58 by roguigna         ###   ########.fr       */
+/*   Updated: 2024/09/24 14:45:22 by roguigna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "HumanB.hpp"
-
-// HumanB::HumanB(std::string	name, Weapon *weapon) : _name(name),  _weapon(weapon)
-// {
-// 	std::cout << "Human B named " << name << "has been created" << std::endl;
-// }
 
 HumanB::HumanB(std::string	name)
 {
--- cpp-module-01/ex03/main.cpp
+++ cpp-module-01/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: roguigna <roguigna@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/17 17:52:19 by roguigna          #+#    #+#             */
-/*   Updated: 2024/07/24 12:12:21 by roguigna         ###   ########.fr       */
+/*   Updated: 2024/09/24 14:47:58 by roguigna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,14 @@
 
 int main()
 {
-		// {
-		// Weapon club = Weapon("crude spiked club");
-		// HumanA bob("Bob", club);
-		// bob.attack();
-		// club.setType("some other type of club");
-		// bob.attack();
-		// }
+		{
+		Weapon club = Weapon("crude spiked club");
+		HumanA bob("Bob", club);
+		bob.attack();
+		club.setType("some other type of club");
+		bob.attack();
+		}
+		std::cout << "-----------------------------" << std::endl;
 		{
 		Weapon club = Weapon("crude spiked club");
 		HumanB jim("Jim");
