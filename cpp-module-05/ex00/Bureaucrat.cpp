e5946574cb06ebf8e1435dea2b2de480d3fdf2ab
romainguign <guignard.romain@protonmail.com>
2024-10-11 13:59:27 +0200

    ex00 OK

---
+++ cpp-module-05/ex00/Bureaucrat.cpp
@@ -0,0 +1,72 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.cpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: roguigna <roguigna@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/08 13:49:51 by roguigna          #+#    #+#             */
+/*   Updated: 2024/10/11 13:57:17 by roguigna         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Bureaucrat.hpp"
+
+Bureaucrat::Bureaucrat(std::string const & name, int grade) : _name(name){
+	std::cout << "Bureaucrat : Constructor called" << std::endl;
+	if (grade < 1)
+		throw GradeTooLowException();
+	if (grade > 150)
+		throw GradeTooHighException();
+	_grade = grade;
+}
+
+Bureaucrat::Bureaucrat(const Bureaucrat &c) : _name(c._name) {
+	*this = c;
+	std::cout << "Bureaucrat : Copy constructor called" << std::endl;
+}
+
+Bureaucrat &Bureaucrat::operator=(const Bureaucrat &c) {
+	std::cout << "Copy assignment operator called " << std::endl;
+	if (this != &c)
+		_grade = c._grade;
+	return (*this);
+}
+
+void	Bureaucrat::decrementGrade() {
+	if (this->_grade == 1)
+		throw GradeTooLowException();
+	this->_grade--;
+}
+
+void	Bureaucrat::incrementGrade() {
+	if (this->_grade == 150)
+		throw GradeTooHighException();
+	this->_grade++;
+}
+
+const std::string Bureaucrat::getName() const{
+	return (this->_name);
+}
+unsigned int Bureaucrat::getGrade() const {
+	return (this->_grade);
+}
+
+std::ostream& operator<<(std::ostream& os, const Bureaucrat& obj)
+{
+	os << obj.getName() << ", bureaucrat grade " << obj.getGrade() << ".";
+    return (os);
+}
+
+
+const char *Bureaucrat::GradeTooHighException::what() const throw(){
+	return ("Error: Grade too high");
+}
+
+const char *Bureaucrat::GradeTooLowException::what() const throw(){
+	return ("Error: Grade too low");
+}
+
+Bureaucrat::~Bureaucrat(){
+	std::cout << "Bureaucrat : Destructor called " << std::endl;
+}
---
+++ cpp-module-05/ex00/main.cpp
@@ -0,0 +1,68 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: roguigna <roguigna@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/24 14:37:16 by roguigna          #+#    #+#             */
+/*   Updated: 2024/10/11 13:56:56 by roguigna         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Bureaucrat.hpp"
+
+int main()
+{
+    std::cout << "-------- test constructor --------" << std::endl;
+    try{
+        Bureaucrat eddy("Eddy", 151);
+        std::cout << eddy << std::endl;
+    }
+    catch (std::exception &e){
+        std::cerr << e.what() << std::endl;
+    }
+    try{
+        Bureaucrat eddy("Eddy", 0);
+        std::cout << eddy << std::endl;
+    }
+    catch (std::exception &e){
+        std::cerr << e.what() << std::endl;
+    }
+    try{
+        Bureaucrat eddy("Eddy", -1);
+        std::cout << eddy << std::endl;
+    }
+    catch (std::exception &e){
+        std::cerr << e.what() << std::endl;
+    }
+    try{
+        Bureaucrat eddy("Eddy", 42);
+        std::cout << eddy << std::endl;
+    }
+    catch (std::exception &e){
+        std::cerr << e.what() << std::endl;
+    }
+    std::cout << "\n-------- test increment --------" << std::endl;
+    try{
+        Bureaucrat eddy("Eddy", 145);
+        for (int i = 0; i < 10; i++){
+            eddy.incrementGrade();
+            std::cout << eddy << std::endl;
+        }
+    }
+    catch (std::exception &e){
+        std::cerr << e.what() << std::endl;
+    }
+    std::cout << "\n-------- test decrement --------" << std::endl;
+    try{
+        Bureaucrat eddy("Eddy", 5);
+        for (int i = 0; i < 10; i++){
+            eddy.decrementGrade();
+            std::cout << eddy << std::endl;
+        }
+    }
+    catch (std::exception &e){
+        std::cerr << e.what() << std::endl;
+    }
+}
