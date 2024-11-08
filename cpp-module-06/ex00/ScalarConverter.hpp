0eaba72455cb2c639a33b2a522234a330423f8cc
romainguign <guignard.romain@protonmail.com>
2024-11-08 13:01:45 +0100

    just started

---
+++ cpp-module-06/ex00/ScalarConverter.hpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScalarConverter.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: roguigna <roguigna@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/07 17:06:54 by roguigna          #+#    #+#             */
+/*   Updated: 2024/11/07 17:08:12 by roguigna         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SCALAR_CONVERTER_HPP
+# define SCALAR_CONVERTER_HPP
+
+# include <iostream>
+
+class ScalarConverter
+{
+	private:
+		ScalarConverter();
+	public:
+};
+
+#endif
