1123defa1ba93cd2d820f293631b02c432ed2213
romainguign <guignard.romain@protonmail.com>
2024-11-19 17:21:28 +0100

    finished

--- cpp-module-09/ex00/BitcoinExchange.cpp
+++ cpp-module-09/ex00/BitcoinExchange.cpp
@@ -6,7 +6,7 @@
 /*   By: roguigna <roguigna@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/14 16:45:40 by roguigna          #+#    #+#             */
-/*   Updated: 2024/11/18 17:55:04 by roguigna         ###   ########.fr       */
+/*   Updated: 2024/11/19 16:53:02 by roguigna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,6 +35,31 @@ void BitcoinExchange::saveData()
 	}
 }
 
+static bool isLeapYear(int year) {
+	return (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0));
+}
+
+static bool isValidDate(int year, int month, int day) {
+	if (year < 1 || month < 1 || month > 12 || day < 1) {
+		return false;
+	}
+	int daysInMonth[] = { 31, isLeapYear(year) ? 29 : 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+	return day <= daysInMonth[month - 1];
+}
+
+static bool checkDate(const std::string& dateStr) {
+	int year, month, day;
+	char delimiter1, delimiter2;
+	std::istringstream dateStream(dateStr);
+
+	if (dateStream >> year >> delimiter1 >> month >> delimiter2 >> day) {
+		if (delimiter1 == '-' && delimiter2 == '-') {
+			return isValidDate(year, month, day);
+		}
+	}
+	return false;
+}
+
 static void parseDate(std::string &date)
 {
 	if (date.empty())
@@ -43,6 +68,8 @@ static void parseDate(std::string &date)
 		throw std::runtime_error("invalid date format");
 	if (date[4] != '-' || date[7] != '-')
 		throw std::runtime_error("invalid date format");
+	if (!checkDate(date))
+		throw std::runtime_error("invalid date");
 }
 
 static void parseLine(std::string line, std::string &date, double &value)
@@ -54,6 +81,10 @@ static void parseLine(std::string line, std::string &date, double &value)
 		throw std::runtime_error("invalid separator");
 	date = line.substr(0, line.find(" | "));
 	parseDate(date);
+	if (line.find(" | ") + 3 >= line.size())
+		throw std::runtime_error("no value");
+	if ((line.substr(line.find(" | ") + 3)).find_first_not_of("0123456789.") != std::string::npos)
+		throw std::runtime_error("invalid value");
 	value = std::strtod((line.substr(line.find(" | ") + 3)).c_str(), NULL);
 	if (value < 0)
 		throw std::runtime_error("not a positive number");
@@ -75,7 +106,10 @@ void BitcoinExchange::displayExchangeRate(std::string &date, double value) const
 			if (it->first > date)
 			{
 				if (it == _data.begin())
-					throw std::runtime_error("no data available");
+				{
+					rate = value * it->second;
+					break;
+				}
 				it--;
 				rate = value * it->second;
 				break;
@@ -95,6 +129,10 @@ void BitcoinExchange::convertExchanges(char *filename)
 	
 	std::string date;
 	double value;
+	
+	std::getline(file, line);
+	if (line != "date | value")
+		throw std::runtime_error("invalid header");
 	while (std::getline(file, line))
 	{
 		try {
